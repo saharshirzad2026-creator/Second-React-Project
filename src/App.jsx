23c8280e71f24ca620b2ebd470d81c907c7da8d9
saharshirzad2026-creator <saharshirzad2026@gmail.com>
2026-04-14 23:29:10 -0700

    I make some changes

--- src/App.jsx
+++ src/App.jsx
@@ -10,3 +10,23 @@ export default function App(){
   const [sunsetHour, setSunsetHours] = useState("");
   const [sunsetMinute, setSunsetMinute] = useState("");
   const [sunsetSeconnds, setSunsetSeconnds] = useState("");
+  useEffect(()=>{
+    async function getWeather(){
+      const data = await fetch("https://api.openweathermap.org/data/2.5/weather?q=Ghazni&appid=c06cd9456c06e943016cd578f35487fe");
+      const jawab = await data.json();
+      setWeatherData(jawab);
+      const sunRiseDate = new Date(jawab.sys.sunrise * 1000);
+      const sunRiseHour = sunRiseDate.getHours();
+      const sunRiseMinute = sunRiseDate.getMinutes()
+      const sunRiseSeconds = sunRiseDate.getSeconds();
+      setHours(sunRiseHour);
+      setMinute(sunRiseMinute)
+      setSeconds(sunRiseSeconds);
+      const sunSetDate = new Date(jawab.sys.sunset * 1000);
+      const sunSetHour = sunSetDate.getHours()
+      const sunSetMinute = sunSetDate.getminutes()
+      const sunSetSeconds= sunSetDate.getSeconds()
+      setSunsetHours(sunSetHour);
+      setSunsetMinute(sunSetMinute);
+      setSunsetSeconnds(sunSetSeconds);
+    }
